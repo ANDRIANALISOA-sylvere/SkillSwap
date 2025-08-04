99b28fbe92d495092261c7ab651fe6de4a42629e
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2025-08-04 18:20:19 +0300

    [FEAT] : create modules (users,skills,exchanges and reviews)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SkillsModule } from './skills/skills.module';
+import { ExchangesModule } from './exchanges/exchanges.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, SkillsModule, ExchangesModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
